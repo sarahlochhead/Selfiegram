8a30acf80c5955034b498830f24e53d0d82727e1
Sarah Lochhead <sarahlochhead@gmail.com>
2017-11-30 21:03:46 -0500

    End of W5D2 not sure I got it to save an activity type or not :/

--- Selfiegram/SelfieTableViewCell.swift
+++ Selfiegram/SelfieTableViewCell.swift
@@ -14,6 +14,7 @@ class SelfieTableViewCell: UITableViewCell {
     @IBOutlet weak var selfieImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
     
     var post: Post? {
         
@@ -32,6 +33,22 @@ class SelfieTableViewCell: UITableViewCell {
                 
                 usernameLabel.text = post.user.username
                 commentLabel.text = post.comment
+                
+                likeButton.isSelected = false
+                
+                let query = post.likes.query()
+                query.findObjectsInBackground(block: { (users, error) -> Void in
+                    if let users = users as? [PFUser]{
+                        for user in users {
+                            // If we find that the current user's objectId in our collection
+                            // of likes we set the likeButton to selected
+                            // objectId is a great way to compare if two objects are equal
+                            if user.objectId == PFUser.current()?.objectId {
+                                self.likeButton.isSelected = true
+                            }
+                        }
+                    }
+                })
             }
         }
     }
@@ -68,7 +85,7 @@ class SelfieTableViewCell: UITableViewCell {
                     }
                 })
             }
-            
+                
             else { // like button has been deselected and we should remove the like
                 
                 // PFRelation also has a useful method called removeObject that removes
@@ -77,6 +94,27 @@ class SelfieTableViewCell: UITableViewCell {
                 post.saveInBackground(block: { (success, error) -> Void in
                     if success {
                         print("like from user successfully removed")
+                        
+                        //PFQuery to find the like activity
+                        if let activityQuery = Activity.query(){
+                            activityQuery.whereKey("post", equalTo: post)
+                            activityQuery.whereKey("user", equalTo: user)
+                            activityQuery.whereKey("type", equalTo: "like")
+                            activityQuery.findObjectsInBackground(block: { (activities, error) -> Void in
+                                
+                                
+                                // You should only have one like activity from a user
+                                // but this is code is being safe and checking for one or multiple instances
+                                // and then deleting all of them
+                                if let activities = activities {
+                                    for activity in activities {
+                                        activity.deleteInBackground(block: { (success, error) -> Void in
+                                            print("deleted activity")
+                                        })
+                                    }
+                                }
+                            })
+                        }
                     }else{
                         print("error is \(error)")
                     }
